7493903d1f0f7448f126e1b78698881eced63272
jadins24 <jadinesh04@gmail.com>
2023-04-15 12:02:07 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var i=1;
+
+        console.log("Dins")
+	function navbar(){
+		i++;
+		if(i%2==0){
+			document.querySelector('.nav-link').style='display:flex;width: 50%;flex-direction: column;position: absolute;background: white;right: 0;top: 50px;'
+		
+		}
+		if(i%2!=0){
+			document.querySelector('.nav-link').style='display:none'
+		}
+		console.log('hi')
+	}
+	function fun(){
+		if(document.querySelector('.nav-link').style.display=='flex'){
+			document.querySelector('.nav-link').style='display:none'
+			i=1
+		}
+        console.log("Dins")
+	}
